e039c98b0e279201acbe50358a68a29e14cf63a3
Bakare Halimat <halimatbakare10@gmail.com>
2022-08-09 21:57:02 +0100

    Dbcontext

---
+++ eTicket/Models/Producer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicket.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int? Id { get; set; }
+
+        public string? ProfilePicture { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string? Bio { get; set; }
+        //Relationship
+        public List<Movie>? Movies { get; set; }
+    }
+}
